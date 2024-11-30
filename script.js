f901f0e9511920203a36ebcbd8c39e620eee971e
rahman <hrepunzel@gmail.com>
2024-11-30 19:06:44 +0700

    update script 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const input = document.getElementById("userInput"); // digunakan untuk mencari elemen dalam HTML berdasarkan 'id' tertentu.
-const item = document.getElementsByTagName("li"); // menambahkan item, jika ada beberapa elemen '<li></li>'
-const ul = document.querySelector("ul"); // mencari lemen yang cocok berdasarkan selector CSS, jika ada elemen seperti '<ul></ul>'
+const input = document.getElementById("userInput"); 
+const item = document.getElementsByTagName("li"); 
+const ul = document.querySelector("ul"); 
 
 const enter = document.getElementById("enter");
 enter.onclick = function() {
@@ -14,7 +14,7 @@ li.addEventListener("click", function() {
   li.classList.toggle("done");
 });
 
-export const deletes = document.getElementById("deleteItem");
+const deletes = document.getElementById("deleteItem");
 deletes.onclick = () => {
   li.classList.toggle("delete");
   ul.removeChild(li);
