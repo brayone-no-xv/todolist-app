66988c192e33ef085c8124c40860bd30f7bd582d
rahman <hrepunzel@gmail.com>
2024-12-13 21:58:52 +0700

    update all

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // ambil elemen DOm
-const BtnAddTask = document.getElementById("InputData");
+const InputTask = document.getElementById("InputData");
 const Tasklist = document.querySelector("ul");
 const TaskItems = document.getElementsByTagName("li");
-const InputTask = document.getElementById("id_item");
+const BtnInputTask = document.getElementById("id_item");
 const BtnDeleteTask = document.getElementById("delete_item");
 
 // Nambah Item
-InputTask.ondblclick = () => {
-  const newLi = document.createElement("li");
-  ulTask.appendChild(newLi);
-  newItem.appendChild(document.createTextNode(input.value));
-  input.value = "";
-  console.alert("tes");
+BtnInputTask.ondblclick = () => {
+  const NewItem = document.createElement("li");
+  Tasklist.appendChild(NewItem);
+  NewItem.appendChild(document.createTextNode(InputTask.value));
+  InputTask.value = "";
+  console.alert("Berhasil nambah tugas!");
 };
 
-// Hapus Item
+// Hapus Item setelah nambah item
 BtnDeleteTask.onclick = () => {
   Tasklist.removeChild(TaskItems[0]);
 };
