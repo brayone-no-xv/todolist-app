724ce71bcf15026b7cafc2671723cdf7950477b0
rahman <hrepunzel@gmail.com>
2024-12-30 18:20:02 +0700

    refactore code

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // ambil elemen DOm
-const InputTask = document.getElementById("InputData");
+const inputTeks = document.getElementById("InputData");
 const Tasklist = document.querySelector("ul");
 const TaskItems = document.getElementsByTagName("li");
 const BtnInputTask = document.getElementById("id_item");
 const BtnDeleteTask = document.getElementById("delete_item");
 
 // Nambah Item
-BtnInputTask.ondblclick = () => {
+BtnInputTask.addEventListener("click", () => {
   const NewItem = document.createElement("li");
-  Tasklist.appendChild(NewItem);
-  NewItem.appendChild(document.createTextNode(InputTask.value));
-  InputTask.value = "";
-  console.alert("Berhasil nambah tugas!");
-};
+  Tasklist.appendChild(NewItem).append(
+    document.createTextNode(inputTeks.value)
+  );
+  inputTeks.value += Tasklist.target.value;
+});
 
 // Hapus Item setelah nambah item
 BtnDeleteTask.onclick = () => {
+  BtnDeleteTask.classList.add("button");
   Tasklist.removeChild(TaskItems[0]);
 };
